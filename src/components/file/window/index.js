c8d178aaf934dae57488159538f7cfbc0db7ea41
Daniel Tuna <danieltuna97@gmail.com>
2022-06-02 01:36:30 +0300

    added theme support for file window / css fixes

--- src/components/file/window/index.js
+++ src/components/file/window/index.js
@@ -1,21 +1,29 @@
 import { h } from 'preact';
+import { useContext } from 'preact/hooks';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   handleFileClose,
   handleFileMinimize
 } from 'utils/functions/handleFiles';
 
-import Icon from '../../material-icon/index';
+import Icon from 'components/material-icon';
+
+import Theme from 'utils/contexts/Theme';
 import style from './style.css';
+
+function replaceEOLWithBrTag(content) {
+  return content.replace(/(?:\r\n|\r|\n)/g, '<br>');
+}
 
 function WindowFrame() {
   const dispatch = useDispatch();
   const file = useSelector((state) => state.file.fileInfo);
+  const { theme, setTheme } = useContext(Theme)
 
   return (
-    <div class={`${style.innerWindow}`}>
-      <div class={style.innerWindowBox}>
-        <div class={style.innerWindowNav}>
+    <div class={`${theme === "light" && style.darkText} ${style.innerWindow}`}>
+      <div class={`${theme === "light" && style.lightBoxBg} ${style.innerWindowBox}`}>
+        <div class={`${theme === "light" && style.lightNavBg} ${style.innerWindowNav}`}>
           <div class={style.fileName}>
             {file.Type}s - {file.Key}
           </div>
@@ -25,12 +33,16 @@ function WindowFrame() {
             <Icon type="close" class={`${style.icon} ${style.exitIcon}`} onClick={() => handleFileClose(file, dispatch)} />
           </div>
         </div>
-        <div class={style.innerWindowContent}>
-          {file.Type === 'image' && (
-            <img class={style.innerWindowImg} src={file.ResourcePath} alt={`${file.Key} image`} />
-          )}
-          {file.Type !== 'image' && <h5>{file.Contents}</h5>}
-        </div>
+        {file.Type === 'image' && (
+          <div class={style.innerImageContent}>
+            <img class={style.innerImg} src={file.ResourcePath} alt={`${file.Key} image`} />
+          </div>
+        )}
+        {file.Type !== 'image' && (
+          <div class={style.innerTextContent}>
+            <div dangerouslySetInnerHTML={{ __html: replaceEOLWithBrTag(file.Contents) }}></div>
+          </div>
+        )}
       </div>
     </div>
   );
