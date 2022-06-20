a2d0b91904293ac126c5d4d6355f4ab2930d3aa3
Daniel Tuna <danieltuna97@gmail.com>
2022-06-20 22:45:58 +0300

    fix prod baseurl

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("cypress");
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:8080'
+        baseUrl: 'https://idratherprogram.com'
     }
 });
